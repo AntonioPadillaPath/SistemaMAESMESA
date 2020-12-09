163e7f6d82526bcfd1f45f67ab0f886c85cba4b7
AntonioPadillaPath <66740578+AntonioPadillaPath@users.noreply.github.com>
2020-12-09 10:30:08 -0600

    Add files via upload

---
+++ Entidades/Guardar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Guardar
+    {
+        private string nocot;
+        private string cliente;
+        private string codigo;
+        private string concepto;
+        private string cantidad;
+        private string precio;
+        private string importe;
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Concepto { get => concepto; set => concepto = value; }
+        public string Cantidad { get => cantidad; set => cantidad = value; }
+        public string Precio { get => precio; set => precio = value; }
+        public string Importe { get => importe; set => importe = value; }
+        public string Nocot { get => nocot; set => nocot = value; }
+        public string Cliente { get => cliente; set => cliente = value; }
+    }
+}
